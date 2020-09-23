c31513e9aa071ed21ec0e7d9942963a1de9f4338
DmitriyGalyanov <dimagalyanov@gmail.com>
2020-09-23 14:56:26 +0300

    v0.5.1
    edit mainReducer,
    edit allNews_reducer

--- lib/redux/MainReducer.dart
+++ lib/redux/MainReducer.dart
@@ -6,19 +6,20 @@ import 'package:flutter_news_app/redux/bookmarkedNews/bookmarkedNews_reducer.dar
 
 import 'allNews/allNews_reducer.dart';
 
-// AppState mainReducer(AppState state, dynamic action) => AppState(
-//   allNewsState: allNewsReducer(state.allNewsState, action), // ????
-// );
+// AppState mainReducer(AppState state, dynamic action) {
+//   if (action is SetAllNewsStateAction) {
+//     final nextAllNewsState = allNewsReducer(state.allNewsState, action);
+//     return state.copyWith(allNewsState: nextAllNewsState);
+//   } else if (action is ToggleIsBookmarked) {
+//     print(action.chosenNewsPiece.content); print('test');
+//     final nextBookmarkedNewsState = bookmarkedNewsReducer(state.bookmarkedNewsState, action);
+//     return state.copyWith(bookmarkedNewsState: nextBookmarkedNewsState);
+//   }
+//
+//   return state;
+// }
 
-AppState mainReducer(AppState state, dynamic action) {
-  if (action is SetAllNewsStateAction) {
-    final nextAllNewsState = allNewsReducer(state.allNewsState, action);
-    return state.copyWith(allNewsState: nextAllNewsState);
-  } else if (action is ToggleIsBookmarked) {
-    print(action.chosenNewsPiece.content); print('test');
-    final nextBookmarkedNewsState = bookmarkedNewsReducer(state.bookmarkedNewsState, action);
-    return state.copyWith(bookmarkedNewsState: nextBookmarkedNewsState);
-  }
-
-  return state;
-}
+AppState mainReducer(AppState state, dynamic action) => AppState(
+  allNewsState: allNewsReducer(state.allNewsState, action),
+  bookmarkedNewsState: bookmarkedNewsReducer(state.bookmarkedNewsState, action),
+);
--- lib/redux/allNews/allNews_reducer.dart
+++ lib/redux/allNews/allNews_reducer.dart
@@ -2,8 +2,20 @@
 
 import 'package:flutter_news_app/redux/allNews/allNews_state.dart';
 import 'package:flutter_news_app/redux/allNews/allNews_actions.dart';
+import 'package:redux/redux.dart';
 
-AllNewsState allNewsReducer(
+// AllNewsState allNewsReducer(AllNewsState prevState, action) {
+//   if (action is SetAllNewsStateAction) {
+//     final payload = action.allNewsState;
+//     return prevState.copyWith(
+//         isLoading: payload.isLoading,
+//         isError: payload.isError,
+//         allNewsList: payload.allNewsList);
+//   }
+//   return prevState;
+// }
+
+AllNewsState setAllNewsStateReducer(
     AllNewsState prevState, SetAllNewsStateAction action) {
   final payload = action.allNewsState;
   return prevState.copyWith(
@@ -11,3 +23,7 @@ AllNewsState allNewsReducer(
       isError: payload.isError,
       allNewsList: payload.allNewsList);
 }
+
+Reducer<AllNewsState> allNewsReducer = combineReducers<AllNewsState>([
+  new TypedReducer<AllNewsState, SetAllNewsStateAction>(setAllNewsStateReducer),
+]);
